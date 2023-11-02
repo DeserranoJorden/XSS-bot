0c34c2cf269646d8180126b214d3d4a835600fba
DeserranoJorden <jordendeserrano@hotmail.com>
2023-11-02 15:56:02 +0100

    Initial test

---
+++ XSS_bot.js
@@ -0,0 +1,39 @@
+const puppeteer = require('puppeteer');
+
+const URL = "http://192.168.1.108:8123/users";
+const FLAG = "HTF{You don't even know what afraid is}";
+const JWT = "eyJpdiI6IlArWllsNjJWbnU5RElWT1lLaTJCSFE9PSIsInZhbHVlIjoiNDJLRnF0TXNkYjZGY2NJTWtoaHRlOVIyWENTTXVXaDNJZ3NnVERYN2lcLzdmVmJ1UVFUYTZ3NEErdXF6NitpYXQiLCJtYWMiOiIzZGI1NmIzNTc3NTI4MGE1YzcyZDgzNzFhZGIyMWY4MjZlOTgxYmQ0NWViYjA2ZjU1NjA3YWZmODE5Mzg1NzY2In0%3D";
+
+(async () => {
+    const browser = await puppeteer.launch({
+        args: [
+            '--disable-web-security',
+            '--ignore-certificate-errors',
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+        ],
+        headless: true,
+        ignoreHTTPSErrors: true,
+    });
+    const page = await browser.newPage();
+    await page.setCookie({
+        "name": "flag",
+        "value": FLAG,
+        "domain": "192.168.1.108",
+        "path": "/",
+        "httpOnly": false,
+        "secure": false,
+    });
+    await page.setCookie({
+        "name": "jumanji_fan_forum_session",
+        "value": JWT,
+        "domain": "192.168.1.108",
+        "path": "/",
+        "httpOnly": false,
+        "secure": false,
+    });
+    await page.goto(url, { waitUntil: 'networkidle0', timeout: 10000});
+    console.log("[INFO] rendered page: " + url);
+    await page.close();
+    await browser.close();
+})();
