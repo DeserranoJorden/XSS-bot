ae253fb09f6405b56f0ca1db5f3288d111f80fe1
DeserranoJorden <jordendeserrano@hotmail.com>
2023-11-03 10:37:38 +0100

    while loop and sleep timer

--- XSS_bot.js
+++ XSS_bot.js
@@ -7,46 +7,47 @@ const FLAG = "HTF{You+don't+even+know+what+afraid+is}";
 const username = "admin@htf.be";
 const password = "tSFSecurePassword123";
 
-(async () => {
-    const browser = await puppeteer.launch({
-        headless: true,
-        ignoreHTTPSErrors: true,
-        executablePath: '/usr/bin/google-chrome',
-        args: [
-            '--disable-gpu',
-            '--disable-web-security',
-            '--ignore-certificate-errors',
-            '--no-sandbox',
-            '--disable-setuid-sandbox'
-        ]
-    });
-    const page = await browser.newPage();
-    const status = await page.goto("http://" + URL + "/login");
-    console.log(status.status());
+while (True) {
+    (async () => {
+        const browser = await puppeteer.launch({
+            headless: true,
+            ignoreHTTPSErrors: true,
+            executablePath: '/usr/bin/google-chrome',
+            args: [
+                '--disable-gpu',
+                '--disable-web-security',
+                '--ignore-certificate-errors',
+                '--no-sandbox',
+                '--disable-setuid-sandbox'
+            ]
+        });
+        const page = await browser.newPage();
+        const status = await page.goto("http://" + URL + "/login");
 
-    // Login
-    await page.type('#email', username);
-    await page.type('#password', password);
-    await page.click('button.btn-primary');
-    console.log("[INFO] logging in");
-    await page.waitForNavigation();
+        // Login
+        await page.type('#email', username);
+        await page.type('#password', password);
+        await page.click('button.btn-primary');
+        console.log("[INFO] logging in");
+        await page.waitForNavigation();
 
-    // Get cookies
-    const cookies = await page.cookies();
+        // Get cookies
+        const cookies = await page.cookies();
 
-    const page2 = await browser.newPage();
-    cookies.push({
-        "name": "flag",
-        "value": FLAG,
-        "domain": URL,
-        "path": "/",
-        "httpOnly": false,
-        "secure": false,
-    });
-    console.log(cookies);
-    await page2.setCookie(...cookies);
-    await page2.goto("http://" + URL + "/users", { waitUntil: 'networkidle0', timeout: 10000});
-    console.log("[INFO] Going to users page");
-    await page2.close();
-    await browser.close();
-})();
+        const page2 = await browser.newPage();
+        cookies.push({
+            "name": "flag",
+            "value": FLAG,
+            "domain": URL,
+            "path": "/",
+            "httpOnly": false,
+            "secure": false,
+        });
+        await page2.setCookie(...cookies);
+        await page2.goto("http://" + URL + "/users", { waitUntil: 'networkidle0', timeout: 10000});
+        console.log("[INFO] Going to users page");
+        await page2.close();
+        await browser.close();
+    })();
+    await new Promise(resolve => setTimeout(resolve, 300));
+}
