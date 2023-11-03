f8057cf88151395ae691c90b8430f70d9ec1ea60
DeserranoJorden <jordendeserrano@hotmail.com>
2023-11-03 11:02:57 +0100

    loop with timer

--- XSS_bot.js
+++ XSS_bot.js
@@ -7,49 +7,67 @@ const FLAG = "HTF{You+don't+even+know+what+afraid+is}";
 const username = "admin@htf.be";
 const password = "tSFSecurePassword123";
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+let finished = false;
 
-while (true) {
-    (async () => {
-        const browser = await puppeteer.launch({
-            headless: true,
-            ignoreHTTPSErrors: true,
-            executablePath: '/usr/bin/google-chrome',
-            args: [
-                '--disable-gpu',
-                '--disable-web-security',
-                '--ignore-certificate-errors',
-                '--no-sandbox',
-                '--disable-setuid-sandbox'
-            ]
-        });
-        const page = await browser.newPage();
-        const status = await page.goto("http://" + URL + "/login");
-
-        // Login
-        await page.type('#email', username);
-        await page.type('#password', password);
-        await page.click('button.btn-primary');
-        console.log("[INFO] logging in");
-        await page.waitForNavigation();
-
-        // Get cookies
-        const cookies = await page.cookies();
-
-        const page2 = await browser.newPage();
-        cookies.push({
-            "name": "flag",
-            "value": FLAG,
-            "domain": URL,
-            "path": "/",
-            "httpOnly": false,
-            "secure": false,
-        });
-        await page2.setCookie(...cookies);
-        await page2.goto("http://" + URL + "/users", { waitUntil: 'networkidle0', timeout: 10000});
-        console.log("[INFO] Going to users page");
-        await page2.close();
-        await browser.close();
-        await sleep(3000);
-    })();
+const loop = async () => {
+    return new Promise(async (resolve, reject) => {
+        const inner = async() => {
+            if (!finished) {
+                const browser = await puppeteer.launch({
+                    headless: true,
+                    ignoreHTTPSErrors: true,
+                    executablePath: '/usr/bin/google-chrome',
+                    args: [
+                        '--disable-gpu',
+                        '--disable-web-security',
+                        '--ignore-certificate-errors',
+                        '--no-sandbox',
+                        '--disable-setuid-sandbox'
+                    ]
+                });
+                const page = await browser.newPage();
+                await page.goto("http://" + URL + "/login");
+            
+                // Login
+                await page.type('#email', username);
+                await page.type('#password', password);
+                await page.click('button.btn-primary');
+                console.log("[INFO] logging in");
+                await page.waitForNavigation();
+            
+                // Get cookies
+                const cookies = await page.cookies();
+            
+                const page2 = await browser.newPage();
+                cookies.push({
+                    "name": "flag",
+                    "value": FLAG,
+                    "domain": URL,
+                    "path": "/",
+                    "httpOnly": false,
+                    "secure": false,
+                });
+                await page2.setCookie(...cookies);
+                await page2.goto("http://" + URL + "/users", { waitUntil: 'networkidle0', timeout: 10000});
+                page2.on('dialog', async dialog => {
+                    await dialog.accept();
+                    console.log("found an alert(1)");
+                });
+                console.log("[INFO] Going to users page");
+                await page2.close();
+                await browser.close();
+                if (0 = 1) {
+                    finished = true;
+                    resolve();
+                } else {
+                    return inner();
+                }
+                await new Promise(r => setTimeout(r, 2000));
+            }
+        }
+        await inner();
+    })
 }
+await loop();
+
+
